36ba1b10862ccce24990027eb5b8e727c2e18a05
mnl187 <mnl187@gmail.com>
2022-08-17 19:10:14 +0200

    config main site and test .hbs

--- index.js
+++ index.js
@@ -7,8 +7,17 @@ app.use(express.urlencoded({
     extended: true,
 }));
 app.use(express.static('public'));
+app.use(express.json());
 app.engine('.hbs', hbs.engine( {
     extname: '.hbs',
     // helpers: handlebarsHelpers,
 }));
 app.set('view engine', '.hbs');
+
+app.get('/', (req, res) => {
+    res.render('test')
+})
+
+app.listen(3000, 'localhost', () => {
+    console.log('Listening on http://localhost:3000')
+})
