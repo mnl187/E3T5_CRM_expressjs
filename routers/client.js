88fe0f1caf1dbeb1a1eca92f4ac9155592582986
mnl187 <mnl187@gmail.com>
2022-08-23 17:22:24 +0200

    adding edit form

--- routers/client.js
+++ routers/client.js
@@ -36,6 +36,17 @@ clientRouter
         res.render('client/forms/add');
     })
 
+    .get('/form/edit/:id', (req, res) => {
+        res.render('client/forms/edit', {
+            client: db.getOne(req.params.id),
+        });
+    })
+    .put('/:id', (req, res) => {
+        db.update(req.params.id, req.body);
+        res.render('client/modified', {
+            name: req.body.name,
+    })
+
 module.exports = {
     clientRouter,
 }
