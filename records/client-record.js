0ca63b544ae0b7c5f198e99bf3cbe71e67e9f0f7
mnl187 <mnl187@gmail.com>
2022-08-24 06:57:57 +0200

    adding name validation

--- records/client-record.js
+++ records/client-record.js
@@ -1,5 +1,11 @@
 class ClientRecord {
     constructor(obj) {
+        const {name} = obj;
+
+        if (!name || typeof name !== 'string' || name.length < 3) {
+            throw new Error('Imię musi być tekstem o długości min. 3 znaków');
+        }
+
         this.name = obj.name;
         this.mail = obj.mail;
         this.nextContactAt = obj.nextContactAt;
